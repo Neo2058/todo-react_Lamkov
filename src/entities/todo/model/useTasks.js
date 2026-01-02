b5815923315c63b7d680ab8f779d0a32199ec574
Vyacheslav Bobrov <bobrov4709@mail.ru>
2026-01-02 14:33:03 +0300

    Add switch case

--- src/entities/todo/model/useTasks.js
+++ src/entities/todo/model/useTasks.js
@@ -1,9 +1,43 @@
-import {useCallback, useEffect, useMemo, useRef, useState} from "react";
-import tasksAPI from "../../../shared/api/tasks/index.js";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState
+} from "react";
+import tasksAPI from "@/shared/api/tasks/index.js";
+
+const tasksReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_ALL': {
+      return Array.isArray(action.tasks) ? action.tasks : state
+    }
+    case 'ADD': {
+      return [...state, action.task]
+    }
+    case 'TOGGLE_COMPLETE': {
+      const { id, isDone } = action
+
+      return state.map((task) => {
+        return task.id === id ? { ...task, isDone } : task
+      })
+    }
+    case 'DELETE': {
+      return state.filter((task) => task.id !== action.id)
+    }
+    case 'DELETE_ALL': {
+      return []
+    }
+    default: {
+      return state
+    }
+  }
+}
 
 const useTasks = () => {
 
-  const [tasks, setTasks] = useState( [])
+  const [tasks, dispatch] = useReducer(tasksReducer, [])
 
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
@@ -16,7 +50,7 @@ const useTasks = () => {
     const isConfirmed = confirm('Are you sure you want to delete all tasks?')
 
     if (isConfirmed) {
-      tasksAPI.deleteAll(tasks).then(() => setTasks([]))
+      tasksAPI.deleteAll(tasks).then(() => dispatch({ type: 'DELETE_ALL' }))
     }
   }, [tasks])
 
@@ -25,27 +59,18 @@ const useTasks = () => {
       .then(() => {
         setDisappearingTaskId(taskId)
         setTimeout(() => {
-          setTasks(
-            tasks.filter((task) => task.id !== taskId)
-          )
+          dispatch({ type: 'DELETE', id: taskId })
           setDisappearingTaskId(null)
         }, 400)
       })
-  }, [tasks])
+  }, [])
 
   const toggleTaskComplete = useCallback((taskId, isDone) => {
     tasksAPI.toggleComplete(taskId, isDone)
     .then(() => {
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === taskId) {
-            return {...task, isDone}
-          }
-          return task
-        })
-      )
+      dispatch({ type: 'TOGGLE_COMPLETE', id: taskId, isDone})
     })
-  }, [tasks])
+  }, [])
 
   const addTask = useCallback((title) => {
       const newTask = {
@@ -54,7 +79,7 @@ const useTasks = () => {
       }
       tasksAPI.add(newTask)
         .then((addedTask) => {
-          setTasks((prevTasks) => [...prevTasks, addedTask])
+          dispatch({ type: 'ADD', task: addedTask})
           setNewTaskTitle('')
           setSearchQuery('')
           if (newTaskInputRef.current) {
@@ -71,7 +96,10 @@ const useTasks = () => {
     if (newTaskInputRef.current) {
       newTaskInputRef.current.focus()
 
-      tasksAPI.getAll().then(setTasks)
+      tasksAPI.getAll()
+        .then((serverTasks) => {
+          dispatch({type: 'SET_ALL', tasks: serverTasks})
+        })
     }
   }, [])
 
